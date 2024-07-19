53674dfde99fc353cdc38e96cd361c4e1cfe4700
Isha Khandalekar <isha.khandalekar@somaiya.edu>
2024-07-19 10:42:35 +0530

    Added the missing {children} prop to body of layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +26,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        ...
+        {children}
       </body>
     </html>
   );
