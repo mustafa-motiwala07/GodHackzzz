63405e4ccb65d62a24f17a1d9dae01625d5a41c0
mustafa-motiwala07 <mustu.motiwala03@gmail.com>
2024-07-20 01:51:56 +0530

    Made chatbot frontend

---
+++ frontend1/src/lib/utils.ts
@@ -0,0 +1,13 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formattedSourceText(inputText: string) {
+  return inputText
+    .replace(/\n+/g, " ")
+    .replace(/(\w) - (\w)/g, "$1$2")
+    .replace(/\s+/g, " ");
+}
