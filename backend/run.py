28fcbf25069c38819eb25bfbebdacfe411bdacee
Isha Khandalekar <isha.khandalekar@somaiya.edu>
2024-07-21 01:56:23 +0530

    Upload file to database done

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+import os
 
 from model import run_query, update_db
 
@@ -34,18 +36,40 @@ def read_root():
 def test_endpoint(name: str):
     return {"input": name}, 200
 
-@app.post("/update_db/")
-def update_db_endpoint(name: str):
-    update_db(f"./assets/{name}")
-    return {"updated_db": name}, 200
+# @app.post("/update_db/")
+# def update_db_endpoint(name: str):
+#     update_db(f"./files/{name}")
+#     return {"updated_db": name}, 200
 
 @app.post("/run_query/")
 def run_query_endpoint(query: str):
     result = run_query(query)
     return result, 200
 
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'pdf', 'docx', 'ppt'}
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.post("/uploadfile/")
+async def upload_file(file: UploadFile = File(...)):
+    if not os.path.exists("files"):
+        os.makedirs("files")
+
+    file_location = f"files/{file.filename}"
+    
+    if not allowed_file(file.filename):
+        return JSONResponse({"error": "Invalid File Format"}), 400
+    
+    with open(file_location, "wb+") as file_object:
+        file_object.write(file.file.read())
+    
+    update_db(file_location)
+    return {"updated_db": file.filename}, 200 
+    # return JSONResponse(content={"filename": file.filename, "location": file_location})
 
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
--- frontend1/src/components/Navbar.tsx
+++ frontend1/src/components/Navbar.tsx
@@ -1,13 +1,8 @@
 import Link from "next/link";
 
-export const navItems = [
-  {name: 'Home', href:'/'},
-  {name: 'Manage PDF', href:'/components/Test'}
-]
-
 export default function Navbar() {
     return (
-        <header className="fixed top-0 left-0 z-50 w-full bg-background shadow">
+      <header className="fixed top-0 left-0 z-50 w-full bg-background shadow">
       <nav className="container mx-auto flex h-14 items-center justify-between px-4 md:px-6">
         <Link href="/" className="text-lg font-medium" prefetch={false}>
           Home
